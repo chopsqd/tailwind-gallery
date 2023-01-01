315eed949fd4877b5d951c2380d2a82ec240694a
Michael Chop <007micle007@gmail.com>
2023-01-01 19:02:27 +0300

    Added image tags mapping

--- src/App.js
+++ src/App.js
@@ -18,11 +18,13 @@ function App() {
 
     return (
         <div className={"container mx-auto"}>
-            <div className="grid grid-cols-3 gap-4">
-                {images.map(image =>
-                    <ImageCard key={image.id} image={image}/>
-                )}
-            </div>
+            {isLoading
+                ? <h1 className={"text-6xl text-center mx-auto mt-32"}>Loading...</h1>
+                : <div className="grid grid-cols-3 gap-4">
+                    {images.map(image =>
+                        <ImageCard key={image.id} image={image}/>
+                    )}
+                </div>}
         </div>
     );
 }
--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const ImageCard = ({image}) => {
+    const tags = image.tags.split(',')
+
     return (
         <div className={"max-w-sm rounded overflow-hidden shadow-lg"}>
             <img src={image.webformatURL} className="w-full"/>
@@ -24,17 +26,11 @@ const ImageCard = ({image}) => {
                 </ul>
             </div>
             <div className="px-6 py-4">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                #tag1
-            </span>
-                <span
-                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                #tag2
-            </span>
-                <span
-                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                #tag3
-            </span>
+                {tags.map((tag, index) => (
+                    <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                        #{tag}
+                    </span>
+                ))}
             </div>
         </div>
     );
