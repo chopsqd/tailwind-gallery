2299cebaf72a6cd9a1221ee7b0db600a4476db17
Michael Chop <007micle007@gmail.com>
2023-01-01 19:13:48 +0300

    Added an image search component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react";
 import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
 
 function App() {
     const [images, setImages] = useState([])
@@ -14,10 +15,14 @@ function App() {
                 setIsLoading(false)
             })
             .catch(err => console.log(err))
-    }, [])
+    }, [term])
 
     return (
         <div className={"container mx-auto"}>
+            <ImageSearch searchText={(text) => setTerm(text)}/>
+
+            {!isLoading && !images.length && <h1 className={"text-5xl text-center mx-auto mt-32"}>No Images Found</h1>}
+
             {isLoading
                 ? <h1 className={"text-6xl text-center mx-auto mt-32"}>Loading...</h1>
                 : <div className="grid grid-cols-3 gap-4">
