f66b8d3097002618415ac2119bfa59bd6637c75b
Michael Chop <007micle007@gmail.com>
2023-01-01 18:30:28 +0300

    Added pictures fetching

--- src/App.js
+++ src/App.js
@@ -1,39 +1,57 @@
+import {useEffect, useState} from "react";
+
 function App() {
-  return (
-    <div className={"max-w-sm rounded overflow-hidden shadow-lg"}>
-        <img src="https://source.unsplash.com/random" className="w-full"/>
-        <div className="px-6 py-4">
-            <div className="font-bold text-purple-500 text-xl mb-2">
-                Photo by John Doe
+    const [images, setImages] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [term, setTerm] = useState('')
+
+    useEffect(() => {
+        fetch(`https://pixabay.com/api/?key=32534690-ff5e097c501fa09c3d582567e&q=${term}&image_type=photo&pretty=true`)
+            .then(res => res.json())
+            .then(data => {
+                setImages(data.hits)
+                setIsLoading(false)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+    return (
+        <div className={"max-w-sm rounded overflow-hidden shadow-lg"}>
+            <img src="https://source.unsplash.com/random" className="w-full"/>
+            <div className="px-6 py-4">
+                <div className="font-bold text-purple-500 text-xl mb-2">
+                    Photo by John Doe
+                </div>
+                <ul>
+                    <li>
+                        <strong>Views: </strong>
+                        4000
+                    </li>
+                    <li>
+                        <strong>Downloads: </strong>
+                        300
+                    </li>
+                    <li>
+                        <strong>Likes: </strong>
+                        400
+                    </li>
+                </ul>
             </div>
-            <ul>
-                <li>
-                    <strong>Views: </strong>
-                    4000
-                </li>
-                <li>
-                    <strong>Downloads: </strong>
-                    300
-                </li>
-                <li>
-                    <strong>Likes: </strong>
-                    400
-                </li>
-            </ul>
-        </div>
-        <div className="px-6 py-4">
+            <div className="px-6 py-4">
             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
                 #tag1
             </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                <span
+                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
                 #tag2
             </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                <span
+                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
                 #tag3
             </span>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
