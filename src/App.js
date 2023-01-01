2c904f4cda0269b6629c13ab0f0657afc547b650
Michael Chop <007micle007@gmail.com>
2023-01-01 18:36:37 +0300

    Created an image card component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {useEffect, useState} from "react";
+import ImageCard from "./components/ImageCard";
 
 function App() {
     const [images, setImages] = useState([])
@@ -16,39 +17,11 @@ function App() {
     }, [])
 
     return (
-        <div className={"max-w-sm rounded overflow-hidden shadow-lg"}>
-            <img src="https://source.unsplash.com/random" className="w-full"/>
-            <div className="px-6 py-4">
-                <div className="font-bold text-purple-500 text-xl mb-2">
-                    Photo by John Doe
-                </div>
-                <ul>
-                    <li>
-                        <strong>Views: </strong>
-                        4000
-                    </li>
-                    <li>
-                        <strong>Downloads: </strong>
-                        300
-                    </li>
-                    <li>
-                        <strong>Likes: </strong>
-                        400
-                    </li>
-                </ul>
-            </div>
-            <div className="px-6 py-4">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                #tag1
-            </span>
-                <span
-                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                #tag2
-            </span>
-                <span
-                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                #tag3
-            </span>
+        <div className={"container mx-auto"}>
+            <div className="grid grid-cols-3 gap-4">
+                {images.map(image =>
+                    <ImageCard key={image.id} image={image}/>
+                )}
             </div>
         </div>
     );
---
+++ src/components/ImageCard.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const ImageCard = ({image}) => {
+    return (
+        <div className={"max-w-sm rounded overflow-hidden shadow-lg"}>
+            <img src={image.webformatURL} className="w-full"/>
+            <div className="px-6 py-4">
+                <div className="font-bold text-purple-500 text-xl mb-2">
+                    Photo by {image.user}
+                </div>
+                <ul>
+                    <li>
+                        <strong>Views: </strong>
+                        {image.views}
+                    </li>
+                    <li>
+                        <strong>Downloads: </strong>
+                        {image.downloads}
+                    </li>
+                    <li>
+                        <strong>Likes: </strong>
+                        {image.likes}
+                    </li>
+                </ul>
+            </div>
+            <div className="px-6 py-4">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                #tag1
+            </span>
+                <span
+                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                #tag2
+            </span>
+                <span
+                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                #tag3
+            </span>
+            </div>
+        </div>
+    );
+};
+
+export default ImageCard;
